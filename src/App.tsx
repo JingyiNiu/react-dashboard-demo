de4a35eb4dd42d91783effbff7a5298aabf07311
JingyiNiu <niu.jy1203@gmail.com>
2023-03-09 12:17:11 +1300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='mx-auto w-full max-w-sm my-16 text-center'>
+            <h1 className='text-xl poppins-bold mb-4'>Dashboard</h1>
+            <p>please log in</p>
+        </div>
+    );
 }
 
 export default App;
